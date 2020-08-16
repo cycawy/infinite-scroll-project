1a641548027d0a788ceb8f76c216cdcca5c9182d
cycawy <cycawy@gmail.com>
2020-08-16 10:49:05 -0700

    improve code with adding helper function setAttributes for DRY code

--- script.js
+++ script.js
@@ -7,19 +7,35 @@ const count = 10;
 const apiKey = `qBqkKtss4ID7Gk6IOuOhx-AJ_ZTg_5s7nx7QOdcb1uY`;
 const apiUrl =`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Helper function to set Attributes on DOM Elements
+function setAttributes(element, attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key])
+    }
+}
+
 //Create Elements for links and photos, add to DOM
 function displayPhotos(){
     //Run function for each object in photoArray
     photosArray.forEach((photo)=>{
         //Create <a> link to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        //item.setAttribute('href', photo.links.html);
+        //item.setAttribute('target', '_blank');
+       setAttributes(item, {
+            'href': photo.links.html,
+            'target': '_blank',
+        });
         //create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        //img.setAttribute('src', photo.urls.regular);
+        //img.setAttribute('alt', photo.alt_description);
+        //img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            'src': photo.urls.regular,
+            'alt': photo.alt_description,
+            'title': photo.alt_description,
+        });
 
         //put <img> inside <a>, and then put both of them inside imageContainer
         item.appendChild(img);
@@ -34,7 +50,7 @@ async function getPhotos(){
     try{
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        console.log(photosArray);
+        //console.log(photosArray);
         displayPhotos();
     }catch(error){
         console.log("error!", error);
