872fcbf86882b63a86c4524bf430ffa904f8b462
cycawy <cycawy@gmail.com>
2020-08-18 22:20:45 -0700

    add js code for loading more pictures when scrolling to the bottom of the page

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0; 
 let photosArray=[];
 
 // unsplash API
-const count = 10;
+const count = 30;
 const apiKey = `qBqkKtss4ID7Gk6IOuOhx-AJ_ZTg_5s7nx7QOdcb1uY`;
 const apiUrl =`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//check if all images were loaded
+function imageLoaded(){
+    imagesLoaded ++;
+    
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+        console.log("ready is: ", ready);
+       
+    }
+}
 
 //Helper function to set Attributes on DOM Elements
 function setAttributes(element, attributes){
@@ -16,6 +32,8 @@ function setAttributes(element, attributes){
 
 //Create Elements for links and photos, add to DOM
 function displayPhotos(){
+    totalImages = photosArray.length;
+    console.log("totalImages is: ", totalImages);
     //Run function for each object in photoArray
     photosArray.forEach((photo)=>{
         //Create <a> link to unsplash
@@ -36,7 +54,8 @@ function displayPhotos(){
             'alt': photo.alt_description,
             'title': photo.alt_description,
         });
-
+        //event listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         //put <img> inside <a>, and then put both of them inside imageContainer
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -56,6 +75,21 @@ async function getPhotos(){
         console.log("error!", error);
     }
 }
+
+//check to see if scrolling to the bottom of the page, load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        imagesLoaded = 0;
+        //console.log('window.innerHeight is: ', window.innerHeight);
+        //console.log('window.scrollY is: ', window.scrollY);
+        //console.log('window innerHeight+scrollY is: ', window.innerHeight+window.scrollY);
+        //console.log('document.body.offsetHeight is: ', document.body.offsetHeight-1000);
+        getPhotos();
+        console.log('load more');
+    }
+    //console.log('scrolled');
+});
 
 // on load
 getPhotos();
